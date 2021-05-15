b795f71105b5ba763a60bb308ddc73d4fe0d107c
robisz1911 <robisz1911@gmail.com>
2021-05-15 14:20:22 +0200

    Empty activity template

---
+++ app/src/main/java/com/robisz1911/mymemory/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.robisz1911.mymemory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
